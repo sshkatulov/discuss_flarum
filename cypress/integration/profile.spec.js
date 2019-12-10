5f9019ddf81544aaeff239d95104295a727d8206
Sergey <s.shkatulov@gmail.com>
2019-12-10 17:19:40 +0300

    added fixtures and updated names

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -1,47 +1,50 @@
 'use strict';
 /// <reference types="Cypress" />
 
-import credentials from '../fixtures/credentials.json';
-import stubbedResponse from '../fixtures/stubbedResponse.json';
-
 context('Profile', () => {
   beforeEach(() => {
-    cy.login(credentials.user, credentials.password);
+    cy.fixture('credentials').then((credentials) => {
+      cy.login(credentials.user, credentials.password);
+    });
   });
 
   it('Change user profile on front-end only', () => {
-    const fakeBio = stubbedResponse.data.attributes.bio;
+    cy.fixture('stubbedResponse').then((stubbedResponse) => {
+      const stubbedBio = stubbedResponse.data.attributes.bio;
 
-    cy.server();
-    cy.route('/api/posts*').as('getDetails');
-    cy.route('POST', '/api/users/*', stubbedResponse).as('fakePost');
+      cy.fixture('credentials').then((credentials) => {
+        cy.server();
+        cy.route('/api/posts*').as('getDetails');
+        cy.route('POST', '/api/users/*', stubbedResponse).as('stubbedPost');
 
-    cy.visit(`/u/${credentials.user}`);
+        cy.visit(`/u/${credentials.user}`);
 
-    cy.wait('@getDetails').then((response) => {
-      response.response.body.included[0].attributes.bio = fakeBio;
-      return response.response.body;
-    }).as('fakeDetails');
+        cy.wait('@getDetails').then((response) => {
+          response.response.body.included[0].attributes.bio = stubbedBio;
+          return response.response.body;
+        }).as('stubbedDetails');
 
-    cy.get('.UserBio-content')
-        .click();
-    cy.get('.UserBio > .FormControl')
-        .clear()
-        .type(fakeBio)
-        .type('{enter}');
-    cy.wait('@fakePost')
-        .then((request) => {
-          expect(request.requestBody.data.attributes.bio).to.eq(fakeBio);
-        });
+        cy.get('.UserBio-content')
+            .click();
+        cy.get('.UserBio > .FormControl')
+            .clear()
+            .type(stubbedBio)
+            .type('{enter}');
+        cy.wait('@stubbedPost')
+            .then((request) => {
+              expect(request.requestBody.data.attributes.bio).to.eq(stubbedBio);
+            });
 
-    cy.visit('/');
-    cy.route('GET', '/api/posts*', '@fakeDetails')
-        .as('fakeGet');
+        cy.visit('/');
+        cy.route('GET', '/api/posts*', '@stubbedDetails')
+            .as('stubbedGet');
 
-    cy.visit(`/u/${credentials.user}`);
+        cy.visit(`/u/${credentials.user}`);
 
-    cy.wait('@fakeGet');
-    cy.get('.UserBio-content')
-        .should('contain', fakeBio);
+        cy.wait('@stubbedGet');
+        cy.get('.UserBio-content')
+            .should('contain', stubbedBio);
+      });
+    });
   });
 });
